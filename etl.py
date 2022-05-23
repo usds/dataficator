a9ab29a837fbbbc955522a0c911da7d1b4523d1a
Steve Leibman <stephen.g.leibman@omb.eop.gov>
2022-05-23 15:07:38 -0400

    initial ETL test script

---
+++ etl.py
@@ -0,0 +1,18 @@
+import boto3
+import pandas as pd
+
+CLOUDFLARE_ACCOUNT_ID = ''
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+
+s3_bucket_name = 'incoming'
+s3 = boto3.resource('s3',
+                    endpoint_url = f"https://{CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com",
+                    aws_access_key_id = AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
+
+cvses_remote_object = s3.Object(s3_bucket_name,'cvses.csv')
+cvses_response = object.get()
+cvses_df = pd.read_csv(cvses_response['Body'], dtype='string')
+
+print(cvses_df.head())
