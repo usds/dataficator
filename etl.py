b0680463ac242bea878d83700dce3c8d5d9fef83
Steve Leibman <stephen.g.leibman@omb.eop.gov>
2022-05-26 23:07:48 -0400

    Process distance data and inventory, producing many json files

--- etl.py
+++ etl.py
@@ -2,30 +2,34 @@ from __future__ import annotations
 
 import json
 import os
+import re
+import time
 from dataclasses import asdict, dataclass, field
 from datetime import datetime
+from pathlib import Path
 from typing import Dict, Optional
 
 import boto3
-import geopandas
 import pandas as pd
-from geojson import Point as GeoPoint
-from geopandas import points_from_xy
-from shapely.geometry.point import Point
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 CLOUDFLARE_ACCOUNT_ID = os.environ.get("CLOUDFLARE_ACCOUNT_ID")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-METERS_TO_MILES = 1609
-PROJECTION = 'EPSG:3857'  # https://epsg.io/3857
-RADIUS_IN_MI = 2
-ZIP_CODE = '20004'
+# In order to keep retailer names private, these are stored in a ':' delimited environment variable.
+# Example: export RETAILERS="weasleys_wheezes:spacely_sprockets"
+RETAILERS = os.environ.get("RETAILERS").split(':')
 
+DISTANCE_THRESHOLD = 50
+MAX_STORES = 50
+
+INVENTORY_DIR = 'inventory'  # Only used for testing local file writes
 
 s3_incoming_bucket_name = 'incoming'
 s3_retailer_object_name = 'cvses.csv'
-s3_output_bucket_name = 'inventory'
+s3_output_bucket_name = 'private-inventory' # change this to 'inventory' when ready to be public
 
 
 s3 = boto3.resource('s3',
@@ -33,8 +37,10 @@ s3 = boto3.resource('s3',
                     aws_access_key_id=AWS_ACCESS_KEY_ID,
                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
+# S3 filesystem abstraction for reading parquet files
+s3fs = pa.fs.S3FileSystem(access_key=AWS_ACCESS_KEY_ID, secret_key=AWS_SECRET_ACCESS_KEY, 
+                          endpoint_override=f"https://{CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com")
 
-# Output data structures as Python data classes
 
 @dataclass
 class UPCStock:
@@ -42,16 +48,15 @@ class UPCStock:
     level: str
     count: Optional[int]
     updated: str
-
+    
     @classmethod
-    def from_record(cls, record: dict) -> UPCStock:
+    def from_record(cls, record:dict) -> UPCStock:
         return cls(
             upc=record['Fmla_UPC'],
             level=record.get('InStock_Ind'),
             count=record.get('Nbr_UPC_Units'),
-            updated=record['Timestamp_Extract'],
+            updated=record['TimeStamp_Extract'],
         )
-
 
 @dataclass
 class Store:
@@ -63,82 +68,68 @@ class Store:
     state: Optional[str]
     zip: Optional[str]  # I know, but trying to keep names short
     dist: float
-    geometry: dict
     stock: list[UPCStock] = field(default_factory=list)
-
+    
     @classmethod
-    def from_record(cls, record: dict) -> Store:
+    def from_record(cls, record:dict) -> Store:
         return cls(
-            vendor=record['vendor'],
+            vendor=record['Retailer_Name'],
             code=record['Store_Code'],
             phone=record.get('Store_PhoneNbr'),
             web=record.get('Store_Website'),
             addr=record.get('Geo_Full_Address'),
-            state=record.get('Geo_StateAbbrv'),
+            state=record.get('Geo_State_Abbrv'),
             zip=record.get('Geo_Zip_Code'),
-            dist=record.get('distance_from_center'),
-            geometry=dict(GeoPoint(record.get('geometry').coords[0])),
+            dist=record.get('distance'),
             stock=[])
 
     def add_stock(self, record: dict) -> None:
         self.stock.append(UPCStock.from_record(record))
 
+def get_store_inventories(worldwide_inventory_df, store_id, distance):
+    store_inventory_df = worldwide_inventory_df.loc[worldwide_inventory_df['Store_Code']==store_id, :]
+    in_bounds_records = store_inventory_df.set_index(['Retailer_Name', 'Store_Code', 'Fmla_UPC'], 
+                                                     drop=False).to_dict('records')
+    stores = []
+    current_code = None
+    store = None
+    for record in in_bounds_records:
+        record['distance'] = distance
+        store_code = f"{record['Retailer_Name']}-{record['Store_Code']}"
+        if store_code != current_code:
+            store = Store.from_record(record)
+            stores.append(store)
+            current_code = store_code
+        store.add_stock(record)
+    return stores
 
-# Sample retail input data #1
-retailer_remote_object = s3.Object(
-    s3_incoming_bucket_name, s3_retailer_object_name)
-retailer_response = retailer_remote_object.get()
-retailer_frame = pd.read_csv(retailer_response['Body'], dtype='string')
-retailer_frame['vendor'] = 'cvs'
+def write_local_file(filename, string):
+    with open(INVENTORY_DIR / filename, "w") as filehandle:
+        filehandle.write(content)
 
-retailer = geopandas.GeoDataFrame(
-    retailer_frame,
-    geometry=points_from_xy(
-        x=retailer_frame.Geo_Longitude,
-        y=retailer_frame.Geo_Latitude,
-        crs='EPSG:4326',
-    ))
+def write_remote_file(filename, content):
+    output_object = s3.Object(s3_output_bucket_name, filename)
+    result = output_object.put(Body=content)
 
-# Grabbed from HUD's GIS holdings at https://hudgis-hud.opendata.arcgis.com/datasets/d032efff520b4bf0aa620a54a477c70e/explore?showTable=true
-# Note that these leave out empty zips, which for our purposes seems fine?
-zip_centroids = geopandas.read_file(
-    'ZIP_Code_Population_Weighted_Centroids.geojson')
-zip_centroids.set_index('STD_ZIP5', drop=False, inplace=True)
+for retailer in RETAILERS:
+    s3_retailer_inventory_file = retailer + "_master.parquet"
+    s3_retailer_local_stores_file = retailer + "_local_stores.parquet"
 
+    retailer_inventory_table = pq.read_table(f"{s3_incoming_bucket_name}/{s3_retailer_inventory_file}", filesystem=s3fs)
+    retailer_inventory_df = retailer_inventory_table.to_pandas()
 
-def get_distance(frame: geopandas.GeoDataFrame, zips: geopandas.GeoDataFrame, zipcode: str) -> geopandas.GeoDataFrame:
-    frame = frame.to_crs(PROJECTION)
-    zips = zips.to_crs(PROJECTION)
-    center = zips.loc[zipcode]
-    return frame.geometry.distance(center.geometry)
+    retailer_local_stores_table = pq.read_table(f"{s3_incoming_bucket_name}/{s3_retailer_local_stores_file}", filesystem=s3fs)
+    retailer_local_stores_df = retailer_local_stores_table.to_pandas()
 
+    for zip_code in retailer_local_stores_df.index:
+        store_inventories = []
+        for ordinal in range(MAX_STORES):
+            store_id = retailer_local_stores_df.loc[zip_code, pd.IndexSlice[str(ordinal),'store_id']]
+            distance = retailer_local_stores_df.loc[zip_code, pd.IndexSlice[str(ordinal),'distance']]
+            if distance > DISTANCE_THRESHOLD:
+                break
+            store_inventories += get_store_inventories(retailer_inventory_df, store_id, distance)
 
-retailer['distance_from_center'] = get_distance(
-    retailer, zip_centroids, ZIP_CODE)/METERS_TO_MILES
-
-in_bounds_retailer = retailer[retailer.distance_from_center < RADIUS_IN_MI]
-in_bounds_retailer.shape
-
-in_bounds_records = in_bounds_retailer.set_index(
-    ['vendor', 'Store_Code', 'Fmla_UPC'], drop=False).to_dict('records')
-
-stores = []
-current_code = None
-store = None
-for record in in_bounds_records:
-    store_code = f"{record['vendor']}-{record['Store_Code']}"
-    if store_code != current_code:
-        store = Store.from_record(record)
-        stores.append(store)
-        current_code = store_code
-    store.add_stock(record)
-
-inventory_json_string = json.dumps(
-    [asdict(store) for store in stores], indent=2)
-
-output_filename = f"{ZIP_CODE}_{RADIUS_IN_MI}.json"
-output_object = s3.Object(s3_output_bucket_name, output_filename)
-
-result = output_object.put(Body=inventory_json_string)
-
-print(result)
+        output_filename = f"{zip_code}_{DISTANCE_THRESHOLD}.json"
+        content = json.dumps([asdict(store) for store in store_inventories], indent=2)
+        write_remote_file(output_filename, content)
